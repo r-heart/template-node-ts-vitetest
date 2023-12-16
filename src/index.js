7b19f39aca0484111f9d073d6bd0e18ae3237387
r-heart <rachaelkheart@gmail.com>
2023-12-15 16:27:31 -0800

    chore:

---
+++ src/index.js
@@ -0,0 +1,4 @@
+function add2Nums(a, b) {
+    return a + b;
+}
+console.log(add2Nums(1, 2));
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function sub2Nums(a: number, b: number): number {
+	return a - b;
+}
+
+console.log(sub2Nums(1, 2));
